db91adb942b33caf41f5fc71b82abff96dea7f6b
ImmaHarry <haritkhera@gmail.com>
2023-02-17 17:34:29 +0530

    understanding functions

---
+++ r-functions/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    println!("Hello, world!");
+    yo_mama(690000000000000000, 'g');
+}
+
+fn yo_mama(weight: usize, unit: char) {
+    println!(
+        "
+        Yo mama so fattttttt... She weighs {} {}
+    ",
+        weight, unit
+    );
+}
